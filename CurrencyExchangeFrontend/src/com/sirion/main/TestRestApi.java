e9c36efa8453581c8894d32aa971700b50b26bbe
bhupindersinghhhh <139427313+bhupindersinghhhh@users.noreply.github.com>
2023-08-01 09:39:08 +0530

    Add files via upload
    Second

---
+++ CurrencyExchangeFrontend/src/com/sirion/main/TestRestApi.java
@@ -0,0 +1,12 @@
+package com.sirion.main;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class TestRestApi {
+	public static void main(String[] args) {
+
+	}
+}
